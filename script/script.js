7fba53b4fad85614b124282803296cedbc541665
mil-ev <mosbox@bk.ru>
2016-04-14 09:50:19 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function minus(id){
+    var people = document.getElementById(id);
+    people.value--;    
+}
+
+function plus(id){
+    var people = document.getElementById(id);
+    people.value++;    
+}
