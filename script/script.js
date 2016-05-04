366811db86839fbf4173a525f01e1aa4af2a58f4
mil-ev <mosbox@bk.ru>
2016-05-04 14:44:29 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,27 @@
-function minus(id){
+var MinToggle = document.querySelector(".min-toggle");
+var MaxToggle = document.querySelector(".max-toggle");
+
+function minus(id) {
     var people = document.getElementById(id);
-    people.value--;    
+    people.value--;
 }
 
-function plus(id){
+function plus(id) {
     var people = document.getElementById(id);
-    people.value++;    
+    people.value++;
 }
+
+MinToggle.addEventListener('mousedown', function(){
+    MinToggle.classList.add("toggle-active");
+});
+MinToggle.addEventListener('mouseup', function(){
+    MinToggle.classList.remove("toggle-active");
+});
+ 
+MaxToggle.addEventListener('mousedown', function(){
+    MaxToggle.classList.add("toggle-active");
+});
+MaxToggle.addEventListener('mouseup', function(){
+    MaxToggle.classList.remove("toggle-active");
+});
+
